a06f92ea2529a15c78435f4a3cc621177c19be12
brianchemase <brianchemo@gmail.com>
2024-11-24 17:31:32 +0300

    upload the updates

--- resources/views/dashboard/inc/sidebar.blade.php
+++ resources/views/dashboard/inc/sidebar.blade.php
@@ -1,9 +1,9 @@
-<div id="app-sidepanel" class="app-sidepanel"> 
+		<div id="app-sidepanel" class="app-sidepanel"> 
 	        <div id="sidepanel-drop" class="sidepanel-drop"></div>
 	        <div class="sidepanel-inner d-flex flex-column">
 		        <a href="#" id="sidepanel-close" class="sidepanel-close d-xl-none">&times;</a>
 		        <div class="app-branding">
-		            <a class="app-logo" href="{{ route('dash.index') }}"><img class="logo-icon me-2" src="{{asset('dash/assets/images/app-logo.svg')}}" alt="logo"><span class="logo-text">PORTAL</span></a>
+		            <a class="app-logo" href="{{ route('dash.index') }}"><img class="logo-icon me-2" src="{{asset('dash/assets/images/app-logo.svg')}}" alt="logo"><span class="logo-text">SJAK DocShare</span></a>
 	
 		        </div><!--//app-branding-->  
 		        

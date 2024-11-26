93a2ddec12f470f9f73944cb9dde1b69e042c022
brianchemase <brianchemo@gmail.com>
2024-11-26 22:01:12 +0300

    updated files

--- resources/views/dashboard/inc/master.blade.php
+++ resources/views/dashboard/inc/master.blade.php
@@ -50,14 +50,16 @@
 	<script src='https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js'></script>
 	<script src='https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js'></script>
 
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    
 
     
     <!-- Page Specific JS -->
     <script src="{{asset('dash/assets/js/app.js')}}"></script> 
 	<script src="{{asset('dash/dist/script.js')}}"></script>
+
+
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
 </body>
 </html> 
